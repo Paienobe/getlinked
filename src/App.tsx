c477dba54a53129e84ca94c89800ec948c88e970
Progress Aienobe <paienobe@gmail.com>
2023-09-20 23:23:40 +0100

    new routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
+import Register from "./pages/Register/Register";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
