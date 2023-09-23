b1f64e85cb5f081574d8483e758d94aa662224bf
Progress Aienobe <paienobe@gmail.com>
2023-09-23 12:00:01 +0100

    more styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
 import { Link, useLocation } from "react-router-dom";
 import RegisterButton from "../RegisterButton/RegisterButton";
 import "./Header.css";
+import menu from "../../assets/menu.png";
+import closer from "../../assets/Group 1.png";
+import { useState } from "react";
 
 const Header = () => {
   const location = useLocation().pathname;
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <header>
       <nav>
@@ -15,19 +19,29 @@ const Header = () => {
           </Link>
         </section>
 
-        <section className="link_holder">
+        <section className={`link_holder ${showMenu ? "show" : "hide"}`}>
+          <img
+            className="closer"
+            src={closer}
+            onClick={() => setShowMenu(false)}
+            alt=""
+          />
           <ul>
             <li>Timeline</li>
             <li>Overview</li>
             <li>FAQS</li>
             <li className={location == "/contact" ? "contact_link" : ""}>
-              <Link to="/contact">Contact</Link>
+              <Link to="/contact" onClick={() => setShowMenu(false)}>
+                Contact
+              </Link>
             </li>
           </ul>
-          <Link to="/register">
+          <Link to="/register" onClick={() => setShowMenu(false)}>
             <RegisterButton />
           </Link>
         </section>
+
+        <img src={menu} onClick={() => setShowMenu(true)} alt="" />
       </nav>
     </header>
   );
